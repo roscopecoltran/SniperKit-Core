8c439d425e92d8a462a333e0178095239a413dc9
Rosco Pecoltran <username@users.noreply.github.com>
2017-08-23 14:03:18 +0200

    Squashed '.References/src/github.com/mortea15/DAT234_MandatoryBashScripting/' content from commit 275a4d1e6
    git-subtree-dir: .References/src/github.com/mortea15/DAT234_MandatoryBashScripting
    git-subtree-split: 275a4d1e61f156953fd1730baa8b73ae1ee8510b

---
+++ MandatoryBashScripting/README.md
@@ -0,0 +1,11 @@
+# DAT234
+
+This repository contains the files for the course DAT234, Hacking & Scripting
+
+The file(s) for each task are separated into their respective folders.
+
+To run the scripts, enter their folder and type e.g. `./task4.bash`
+
+The solutions I've made for Task 4 and Task 5 include one script using fping to ping the hosts, and one using nmap to achieve the same goal.
+
+All files in the task4 and task5 folders can be run either directly with `./task4.bash` (or `./task4nmap.bash`) or by giving the input upon running the script by typing `echo "192.168.56.1" | ./task4.bash`
---
+++ MandatoryBashScripting/task4/task4.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+# task4.bash
+
+echo -n "Enter an IPv4 address here: " # Request an IP from the user
+read ipaddress # Read and store the input
+
+ipaddr="${ipaddress%.*}" # Remove the last octate from the users input
+alive=0 # Store the number of responding hosts
+dead=0 # Store the number of non-responding hosts
+
+echo "We'll ping all IPs in the range $ipaddr.0 to $ipaddr.255, including $ipaddress"
+
+for ip in {0..255} # Ping every IP in the range 0 - 255
+do
+	fping -c1 -t400 $ipaddr.$ip 2>/dev/null 1>/dev/null # Using fping with 1 ping per address and 150ms timeout
+	if [ "$?" = 0 ] # Response was 0, which means reachable
+	then
+		echo "$ipaddr.$ip is alive!"
+		((alive++))
+	else # Response was not 0, which means unreachable
+		echo "$ipaddr.$ip is unreachable!"
+		((dead++))
+	fi # Finished
+done
+echo "$alive hosts responded / $dead hosts did not respond." # Echo the number of hosts responding/not responding
