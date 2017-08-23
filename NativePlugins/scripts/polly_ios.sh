28e956081bad1931502980dd4835f4d70dc092aa
Rosco Pecoltran <username@users.noreply.github.com>
2017-08-23 20:04:57 +0200

    add missing POLLY toolchains for Xcode 9.0beta (Arkit preview)

--- NativePlugins/scripts/polly_ios.sh
+++ NativePlugins/scripts/polly_ios.sh
@@ -7,15 +7,18 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ## iPhoneOS 
 ## #################################################################
 
+XCODEDIR=$(xcode-select -p)
+
 VALID_ARCHS_IOS=(\
-	"ios-10-3 "
+	"ios-11-0 "
+	" "
 )
 
 ## #################################################################
 ## iPhoneOS / 10.3
 ## #################################################################
 
-POLLY_TOOLCHAIN=${POLLY_TOOLCHAIN:-"ios-10-3"}
+POLLY_TOOLCHAIN=${POLLY_TOOLCHAIN:-"ios-11-0"}
 IOS_TOOLCHAIN=${POLLY_TOOLCHAIN}
-build.py --home=${U3D_PLUGIN_CMAKE_ROOT} --toolchain=ios-10-3 --config Releaase --clear --reconfig --jobs 4
+build.py --home=${U3D_PLUGIN_CMAKE_ROOT} --toolchain=${IOS_TOOLCHAIN} --config Releaase --clear --reconfig --jobs 4
 
